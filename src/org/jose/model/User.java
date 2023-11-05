7e5b83d25819e2009e17c1b62bd2c48d53b975e0
jmsp <jose.sanchezpedrazo@outlook.com>
2023-11-05 12:48:01 +0100

    fat: added toString method

--- src/org/jose/model/User.java
+++ src/org/jose/model/User.java
@@ -1,5 +1,15 @@
 package org.jose.model;
-
+/*
+ * Para crear un proyecto git:
+ * 1- Creamos proyecto java o maven tal cual queramos.
+ * 2- Boton derecho sobre el proyecto - team - share project.
+ * 3- Abrir vista project explorer y editar el .gitignore con los archivos que no queramos sincronizar.
+ * 4- Hacer primer commit pulsando boton derecho sobre proyecto - team - commit.
+ * 
+ * 
+ * Para crear getters y setters de forma automatica asi como un to string, boton derecho sobre la clase - source - Generate ....
+ * 
+ * */
 public class User {
 
 	private String name;
@@ -11,5 +21,9 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + "]";
+	}
 }
