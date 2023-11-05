35dfcf1d98876e11427f887a0bae0ab06520a4f3
jmsp <jose.sanchezpedrazo@outlook.com>
2023-11-05 12:42:18 +0100

    feat: create some getters and setters

--- src/org/jose/model/User.java
+++ src/org/jose/model/User.java
@@ -2,6 +2,14 @@ package org.jose.model;
 
 public class User {
 
-	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
